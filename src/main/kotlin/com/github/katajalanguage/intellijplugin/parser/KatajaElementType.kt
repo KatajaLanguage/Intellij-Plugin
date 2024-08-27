74104cd2c442a7324ff2ac364d64bb09fe646b83
XaverWeste <westxave@kkg-do.eu>
2024-08-27 20:41:38 +0200

    clean up

--- src/main/kotlin/com/github/katajalanguage/intellijplugin/parser/KatajaElementType.kt
+++
@@ -1,7 +0,0 @@
-package com.github.katajalanguage.intellijplugin.parser
-
-import com.github.katajalanguage.intellijplugin.language.KatajaLanguage
-import com.intellij.psi.tree.IElementType
-
-class KatajaElementType(debugName: String): IElementType(debugName, KatajaLanguage.INSTANCE) {
-}
